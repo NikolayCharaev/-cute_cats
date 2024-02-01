41cfbfd42e7a025630008a167fc1e4a4d4afc45f
Good_Cthulhu <hayyabusa_01@mail.ru>
2024-02-01 06:42:56 +0300

    fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,29 +1,23 @@
 'use client';
-import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 const Header = () => {
-  const [navigation, setNavigation] = useState('home');
+  const pathname = usePathname();
   return (
     <div className="bg-[#2196F3]  flex items-center">
       <div className="container mx-auto">
         <div className="flex ">
           <Link
-            onClick={() => {
-              setNavigation('home');
-            }}
             className={` h-[64px] flex justify-center items-center  px-6 ${
-              navigation === 'home' ? 'active' : ''
+              pathname === '/' && 'active'
             } `}
             href="/">
             Все котики
           </Link>
           <Link
-            onClick={() => {
-              setNavigation('favorites');
-            }}
             className={` h-[64px] flex justify-center items-center  px-6 ${
-              navigation === 'favorites' ? 'active' : ''
+              pathname === '/Favorites' && 'active'
             } `}
             href="/Favorites">
             Любимые котики
